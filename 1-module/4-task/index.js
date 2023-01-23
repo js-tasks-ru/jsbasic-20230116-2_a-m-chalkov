a6720076ef94487d82b3f69a139dd05ab26e6845
Artem Chalkov <a.m.chalkov@gmail.com>
2023-01-23 15:24:12 +0000

    task 4 completed

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,22 @@
 function checkSpam(str) {
-  // ваш код...
+  let retval = false;
+  const dict = [];
+  dict.push('1xbet', 'xxx');
+  for (let entry in dict) {
+      if ( str.toLowerCase().indexOf(dict[entry]) != -1 ) {
+          retval = true;
+          break;
+      }
+  }
+
+//  i think that expected solution should be something like that code below, but this is too boring, so i made some investigations to docs and wrote the code above.
+
+//  if ( str.toLowerCase().indexOf('1xbet') != -1 ) {
+//      retval = true;
+//  }
+//  if ( str.toLowerCase().indexOf('xxx') != -1 ) {
+//      retval = true;
+//  }
+
+  return retval;
 }

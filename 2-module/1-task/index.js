4e9727ef864a1a12c9dd320e6cbf622b574dc68d
Artem Chalkov <a.m.chalkov@gmail.com>
2023-01-23 19:33:11 +0000

    2mod task 1 completed

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  sum = 0;
+  for (let entry in salaries) {
+      if (typeof salaries[entry] == 'number' && isFinite(salaries[entry])) {
+          sum = sum + salaries[entry];
+      }
+  }
+  return sum;
 }

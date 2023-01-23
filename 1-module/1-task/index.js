ec9403c56d6c159fc098d9a3ab1c9753e2fb14d2
Artem Chalkov <a.m.chalkov@gmail.com>
2023-01-23 16:21:15 +0000

    tsak 1 completed

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  let f = n;
+  if (n == 0) {
+      f = 1;
+  } else {
+      while (n > 0) {
+          f = f * n;
+          n = n - 1;
+      }
+  }
+  return f;
 }
